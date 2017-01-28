aa309fbe3ce3a3abbc96f153d8f607520285c72a
Matvey-Kuk <motakuk@gmail.com>
2017-01-27 19:17:11 -0800

    Test Travis

--- README.rst
+++ README.rst
@@ -1,6 +1,9 @@
 # Cspark-python is a [cisco spark](https://www.ciscospark.com/) library for bots
 
 [![Build Status](https://travis-ci.org/Matvey-Kuk/cspark-python.svg?branch=master)](https://travis-ci.org/Matvey-Kuk/cspark-python)
+![](https://img.shields.io/pypi/dm/cspark-python.svg)
+![](https://img.shields.io/pypi/v/cspark-python.svg)
+![](https://img.shields.io/pypi/pyversions/cspark-python.svg)
 
 ### Why?
 
--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name='cspark-python',
-    version='0.0.2',
-    description='Python library for Cisco Spark',
-    author='Matvei Kukui',
-    author_email='motakuk@gmail.com',
-    url='https://github.com/Matvey-Kuk/cspark-python',
-    packages=['cspark'],
-    install_requires=[
-        'requests==2.12.5',
-        'peewee==2.8.5'
-    ],
-)
+if __name__ == "__main__":
+
+    with open('README.rst', 'r') as f:
+        long_description = f.read()
+
+    setup(
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+        name='cspark-python',
+        description='Python library for Cisco Spark',
+        long_description=long_description,
+        version='0.0.2',
+        author='Matvei Kukui',
+        author_email='motakuk@gmail.com',
+        url='https://github.com/Matvey-Kuk/cspark-python',
+        packages=['cspark'],
+        install_requires=[
+            'requests==2.12.5',
+            'peewee==2.8.5'
+        ],
+    )
